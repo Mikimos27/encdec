3f5debd40c51b097590eda51be5beba5c03d709d
Mikołaj Modliński <mikimod27@gmail.com>
2025-07-08 23:03:00 +0200

    split files

--- aes_256_gcm.h
+++ aes_256_gcm.h
@@ -13,92 +13,27 @@
 #include <memory>
 
 
-class AES_GCM_256_key{
+class AES_256_GCM_key{
 public:
-    AES_GCM_256_key(const char* aad)
-    : tag{0}, aad{(unsigned char*)aad} {
-        RAND_bytes(key, sizeof(key));
-        genIV();
-    }
-    ~AES_GCM_256_key(){
+    AES_256_GCM_key(const char* aad);
+    ~AES_256_GCM_key();
 
-    }
+    void genIV();
 
-    void genIV(){
-        RAND_bytes(iv, sizeof(iv));
-    }
+    void encrypt(const unsigned char* plain, unsigned char* ciphertext, int length);
+    std::string decrypt(const std::string& ciphertext);
+   
+    const unsigned char* get_tag();
+    const unsigned char* get_key();
+    const unsigned char* get_iv();
 
-    void encrypt(const unsigned char* plain, unsigned char* ciphertext, int length){
-        EVP_CIPHER_CTX* ctx = nullptr;
-        EVP_CIPHER* cipher = nullptr;
-        int outlen = 0;
-        int tmplen = 0;
-        int clen = 0;
-        genIV();
-
-
-        ctx = EVP_CIPHER_CTX_new();
-        cipher = EVP_CIPHER_fetch(NULL, "AES-256-GCM", NULL);
-        if(!ctx || !cipher){
-            std::perror("Fetch failed\n");
-        }
-        int taglen = TAGLEN;
-        int ivlen = IVLEN;
-        int plen = length;//Vunurable???????????????????????????
-        OSSL_PARAM params[] = {
-            OSSL_PARAM_construct_int(OSSL_CIPHER_PARAM_IVLEN, &ivlen),
-            OSSL_PARAM_construct_int(OSSL_CIPHER_PARAM_AEAD_TAGLEN, &taglen),
-            OSSL_PARAM_END
-        };
-
-        if(!EVP_EncryptInit_ex2(ctx, cipher, key, iv, params)){
-            std::perror("Init failed\n");
-        }
-        if(!EVP_EncryptUpdate(ctx, NULL, &outlen, aad, strlen((char*)aad))){
-            std::perror("Init failed\n");
-        }
-        if(!EVP_EncryptUpdate(ctx, ciphertext, &outlen, plain, plen)){
-            std::perror("Init failed\n");
-        }
-        clen = outlen;
-        if(!EVP_EncryptFinal_ex(ctx, ciphertext + outlen, &tmplen)){
-            std::perror("Init failed\n");
-        }
-        clen += tmplen;
-        OSSL_PARAM get_params[] = {
-            OSSL_PARAM_construct_octet_string(OSSL_CIPHER_PARAM_AEAD_TAG, tag, TAGLEN),
-            OSSL_PARAM_END
-        };
-        if(!EVP_CIPHER_CTX_get_params(ctx, get_params)){
-
-        }
-
-        EVP_CIPHER_free(cipher);
-        EVP_CIPHER_CTX_free(ctx);
-    }
-    std::string decrypt(const std::string& ciphertext){
-        using std::size_t;
-        return "TODO";
-        
-    }
-
-    const unsigned char* get_tag(){
-        return tag; 
-    }
-    constexpr int get_taglen(){
+    static constexpr int get_taglen(){
         return TAGLEN;
     }
-    const unsigned char* get_key(){
-        return key; 
-    }
-    constexpr int get_keylen(){
+    static constexpr int get_keylen(){
         return KEYLEN;
     }
-
-    const unsigned char* get_iv(){
-        return iv; 
-    }
-    constexpr int get_ivlen(){
+    static constexpr int get_ivlen(){
         return IVLEN;
     }
 
@@ -112,9 +47,10 @@ private:
     const unsigned char* aad;
 };
 
-class RSA_pair{
 
-};
+
+
+
 
 /*
 int genRSApair(size_t byteKeySize, EVP_PKEY*);

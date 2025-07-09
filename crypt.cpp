5f113375317f6a9d6cd2a82286284fbb7da32df3
Mikołaj Modliński <mikimod27@gmail.com>
2025-07-09 12:10:18 +0200

    cleanup functions and consexpr vars

--- crypt.cpp
+++ crypt.cpp
@@ -3,11 +3,13 @@
 #include <cstdio>
 #include <cstddef>
 #include <string>
+#include <openssl/rand.h>
 
 using std::size_t;
+using uchar = unsigned char;
 
-void print_hex(const char* name, const unsigned char* str, size_t len){
-    printf("%s = ", name);
+void print_hex(const char* name, const uchar* str, size_t len){
+    std::printf("%s = ", name);
     for(size_t i = 0; i < len; i++){
         std::printf("%02x", str[i]);
     }
@@ -16,34 +18,40 @@ void print_hex(const char* name, const unsigned char* str, size_t len){
 
 
 int main(){
-    constexpr int length = 20;
+
+    std::string input;
+    std::getline(std::cin, input);
+    int len = (int)input.length();
+    uchar* in = new uchar[len]{0};
+    uchar* cipher = new uchar[len]{0};
+    uchar* out = new uchar[len]{0};
+    char* outch = new char[len]{0};
+
+
     AES_256_GCM_key aes("ExampleAAD");
-    char from[length] = {0};
-    unsigned char in[length] = {0};
-    std::fgets(from, length, stdin);
-    for(int i = 0; i < length; i++){
-        in[i] = (unsigned char)from[i];
+
+    for(int i = 0; i < len; i++){
+        in[i] = (uchar)input.at(i);
     }
-    unsigned char cipher[length];
-    aes.encrypt(in, cipher, length);
+    aes.encrypt(in, cipher, len);
 
-    print_hex("ciphertext", cipher, length);
-    print_hex("tag", aes.get_tag(), AES_256_GCM_key::get_taglen());
-    print_hex("iv", aes.get_iv(), AES_256_GCM_key::get_ivlen());
-    print_hex("key", aes.get_key(), AES_256_GCM_key::get_keylen());
-
-
-    unsigned char out[length] = {0};
-    char outch[length] = {0};
+    print_hex("ciphertext", cipher, len);
+    print_hex("tag", aes.get_tag(), AES_256_GCM_key::TAGLEN);
+    print_hex("iv", aes.get_iv(), AES_256_GCM_key::IVLEN);
+    print_hex("key", aes.get_key(), AES_256_GCM_key::KEYLEN);
 
 
-    aes.decrypt(cipher, out, length);
+    aes.decrypt(cipher, out, len);
     
-    for(int i = 0; i < length; i++){
+    for(int i = 0; i < len; i++){
         outch[i] = (char)out[i];
     }
     std::cout << "Decrypted: " << outch << '\n';
 
 
+    delete[] in;
+    delete[] cipher;
+    delete[] out;
+    delete[] outch;
     return 0;
 }

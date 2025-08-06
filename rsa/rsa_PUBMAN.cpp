97cb0dce179d1e6a4b1b87c6d5ac848718866aef
Mikołaj Modliński <mikimod27@gmail.com>
2025-08-06 19:48:37 +0200

    valgrind rescue nr1

--- rsa/rsa_PUBMAN.cpp
+++ rsa/rsa_PUBMAN.cpp
@@ -1,9 +1,14 @@
 void RSA_keys::load_pubPEM(const char* filepath){
     std::FILE* fp = nullptr;
     fp = std::fopen(filepath, "r");
+    if(this->pub) _free_key(&this->pub);
     if(fp == NULL){
         throw std::invalid_argument("Can't open file");
     }
+    if(!PEM_read_PUBKEY_ex(fp, &this->pub, NULL, NULL, NULL, NULL)){
+        throw std::invalid_argument("Can't read pub from PEM\n");
+    }
+    std::fclose(fp);
 }
 
 void RSA_keys::write_pubPEM(const char* filepath){
@@ -11,7 +16,9 @@ void RSA_keys::write_pubPEM(const char* filepath){
 
     if(!fp) std::invalid_argument("Bad filepath");
 
-    PEM_write_PUBKEY_ex(fp, this->pub, NULL, NULL);
+    if(!PEM_write_PUBKEY(fp, this->pub)){
+        throw std::invalid_argument("Can't write pub to PEM\n");
+    }
 
     std::fclose(fp);
 }
@@ -45,12 +52,13 @@ void RSA_keys::write_pub_to(std::FILE* const fp){
     PEM_write_PUBKEY_ex(fp, this->pub, NULL, NULL);
 }
 
-EVP_PKEY* RSA_keys::_extract_pub(EVP_PKEY* keypair){
+////////////////////Problem vvvvvvvvvvvvvv
+int RSA_keys::_extract_pub(EVP_PKEY* keypair, EVP_PKEY** pub){
     if (!keypair)
-        return nullptr;
+        return 1;
 
-    OSSL_PARAM *params = nullptr;
     EVP_PKEY *pubkey = nullptr;
+    OSSL_PARAM *params = nullptr;
     EVP_PKEY_CTX *ctx_export = nullptr;
     EVP_PKEY_CTX *ctx_import = nullptr;
 
@@ -89,5 +97,25 @@ EVP_PKEY* RSA_keys::_extract_pub(EVP_PKEY* keypair){
     EVP_PKEY_CTX_free(ctx_export);
     EVP_PKEY_CTX_free(ctx_import);
     OSSL_PARAM_free(params);  // always free exported parameters
+    if(*pub) _free_key(pub);
+    *pub = pubkey;
+    pubkey = nullptr;
+    return 0;
+}
+////////////////////Problem ^^^^^^^^^^^^^^
+/*
+EVP_PKEY* RSA_keys::_extract_pub(EVP_PKEY* keypair){
+    if (!keypair)
+        return nullptr;
+    EVP_PKEY *pubkey = nullptr;
+    BIO* bio = BIO_new(BIO_s_mem());
+    if(!PEM_read_bio_PUBKEY(bio, &keypair)) throw std::invalid_argument("BAD1\n");
+    if(!PEM_write_bio_PUBKEY(bio, pubkey)) throw std::invalid_argument("BAD2\n");
+    PEM_write_PUBKEY(stdout, pubkey);
+
+
+
+    BIO_free(bio);
     return pubkey;
 }
+*/

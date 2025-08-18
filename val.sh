9528db49dce66bbe82c571a544714dc2a1df0287
Mikołaj Modliński <mikimod27@gmail.com>
2025-08-18 16:54:33 +0200

    rm out file

--- val.sh
+++ val.sh
@@ -1,2 +1,2 @@
-make && valgrind --tool=memcheck --leak-check=yes --show-leak-kinds=all --track-origins=yes --log-file=valgrind-out.txt -s ./crypto $* && nvim valgrind-out.txt
+make && valgrind --tool=memcheck --leak-check=yes --show-leak-kinds=all --track-origins=yes --log-file=valgrind-out.txt -s ./crypto $* && nvim valgrind-out.txt && rm valgrind-out.txt
 #--verbose 

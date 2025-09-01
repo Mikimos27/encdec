e369528f76ba306f64ab92f61469601130f9a384
Mikimos27 <mikimod27@gmail.com>
2025-09-01 16:44:08 +0200

    rework for lib

--- val.sh
+++
@@ -1,2 +0,0 @@
-make remake && valgrind --tool=memcheck --leak-check=yes --show-leak-kinds=all --track-origins=yes --log-file=valgrind-out.txt -s ./crypto $* && nvim valgrind-out.txt && rm valgrind-out.txt
-#--verbose 
